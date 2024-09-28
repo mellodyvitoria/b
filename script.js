15f52af58d7ff1cb770508cd365a74bdc4743eb8
mellodyvitoria <mellodyvitoriaa@gmail.com>
2024-09-28 11:49:53 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function continuação () {
+    //continua a musica
+    let continua= prompot ("Borboletinha...");
+    ///solicita continuaçao 
+    let dois= prompot ("Fazendo chocolate...");
+    ///solicita a musica
+    let tres = prompot ("pot pot...");
+    ///solicita musica
+    let quatro = prompot (" Olho de vidro...");
+    ///exibe a musica
+    alert ("Borboletinha..."+ continua+" "+fazendo + chocolate + "+ "+dois+" pot pot"+ "tres" + "olho de vidro" + "quatro");
+    console.log ("continuação:", continua);
+    console.log ("continuação:", dois);
+    console.log ("continuação:", tres);
+    console.log ("continuação:", quatro);
+}
